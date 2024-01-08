6962e2ec41c102bc92f4874cbbf2e86ed9309918
gioannou <dev.gioannou@gmail.com>
2024-01-08 09:22:49 +0200

    Change to rose-pine colorscheme; Remove unused pacakge

--- lua/giorgos/plugins/colorscheme.lua
+++ lua/giorgos/plugins/colorscheme.lua
@@ -1,14 +1,36 @@
 return {
 	{
-		"catppuccin/nvim",
+		"rose-pine/neovim",
 		priority = 1000,
 		config = function()
-			local theme = require("catppuccin")
+			local theme = require("rose-pine")
 
 			theme.setup({
-				flavor = "mocha",
+				disable_background = true,
+				disable_float_background = true,
+				highlight_groups = {
+					GitSignsAdd = { bg = "NONE" },
+					GitSignsChange = { bg = "NONE" },
+					GitSignsDelete = { bg = "NONE" },
+				},
 			})
-			vim.cmd([[colorscheme catppuccin]])
+
+			vim.cmd([[colorscheme rose-pine]])
 		end,
 	},
+	--	{
+	--		"catppuccin/nvim",
+	--		priority = 1000,
+	--		config = function()
+	--			local theme = require("catppuccin")
+	--
+	--			theme.setup({
+	--				flavor = "mocha",
+	--			})
+	--
+	--			vim.cmd([[colorscheme catppuccin]])
+	--			vim.cmd([[highlight Normal guibg=none]])
+	--			vim.cmd([[highlight NonText guibg=none]])
+	--		end,
+	--	},
 }
--- lua/giorgos/plugins/gitsigns.lua
+++ lua/giorgos/plugins/gitsigns.lua
@@ -6,15 +6,17 @@ return {
 
 		gitsigns.setup({
 			signs = {
-				add = { text = "│" },
-				change = { text = "│" },
+				add = { text = "❙" },
+
+				change = { text = "❙" },
 				delete = { text = "_" },
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
-				untracked = { text = "┆" },
+				untracked = { text = "❙" },
 			},
+
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
 			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 			watch_gitdir = {
